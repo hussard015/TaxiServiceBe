f9da6bbd47e4efc37568402cc0647aa0f84f634c
hussard01 <hussard01@gmail.com>
2020-06-27 15:07:28 +0900

    익셉션 핸들러 작성

--- src/main/java/com/dramancompany/taxiServiceBe/common/ErrorResponse.java
+++ src/main/java/com/dramancompany/taxiServiceBe/common/ErrorResponse.java
@@ -4,8 +4,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,11 +18,52 @@ public class ErrorResponse {
     private int status;
     private String message;
     private LocalDateTime timestamp;
+    private List<FieldError> errors;
 
     @Builder
-    public ErrorResponse(int status, String message) {
+    public ErrorResponse(int status, String message, List<FieldError> errors) {
         this.status = status;
         this.message = message;
         this.timestamp = LocalDateTime.now();
+        this.errors = errors;
+    }
+
+    @Getter
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class FieldError {
+        private String field;
+        private String value;
+        private String reason;
+
+        @Builder
+        public FieldError(String field, String value, String reason) {
+            this.field = field;
+            this.value = value;
+            this.reason = reason;
+        }
+
+        public static List<FieldError> of(final String field, final String value, final String reason) {
+            List<FieldError> fieldErrors = new ArrayList<>();
+            fieldErrors.add( FieldError
+                    .builder()
+                    .field(field)
+                    .value(value)
+                    .reason(reason)
+                    .build());
+            return fieldErrors;
+        }
+
+        public static List<FieldError> of(final BindingResult bindingResult) {
+            final List<org.springframework.validation.FieldError> fieldErrors = bindingResult.getFieldErrors();
+            return fieldErrors.stream()
+                    .map(error ->
+                            FieldError
+                                    .builder()
+                                    .field(error.getField())
+                                    .value(error.getRejectedValue() == null ? "" : error.getRejectedValue().toString())
+                                    .reason(error.getDefaultMessage())
+                                    .build())
+                    .collect(Collectors.toList());
+        }
     }
 }
--- src/main/java/com/dramancompany/taxiServiceBe/common/ResponseExceptionHandler.java
+++ src/main/java/com/dramancompany/taxiServiceBe/common/ResponseExceptionHandler.java
@@ -6,16 +6,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
 
 @ControllerAdvice
-public class ResponseExceptionHandler extends ResponseEntityExceptionHandler {
+@Slf4j
+public class ResponseExceptionHandler {
 
-    @ExceptionHandler({IllegalArgumentException.class})
-    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException e) {
+    @ExceptionHandler({IllegalArgumentException.class, IllegalStateException.class})
+    protected ResponseEntity<ErrorResponse> handleIllegalArgumentException(RuntimeException e) {
         ErrorResponse response = ErrorResponse.builder()
                 .status(HttpStatus.BAD_REQUEST.value())
                 .message(e.getMessage())
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("필드 정보가 잘못되었습니다.")
+                .errors(ErrorResponse.FieldError.of(e.getBindingResult()))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({MethodArgumentTypeMismatchException.class})
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        final String value = e.getValue() == null ? "" : e.getValue().toString();
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message("필드 정보가 잘못되었습니다.")
+                .errors(ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode()))
                 .build();
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
