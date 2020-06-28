2dc398fe5b3625a5cffd7c121923046d0f885b39
hussard01 <hussard01@gmail.com>
2020-06-28 14:35:30 +0900

    enum Exception 처리

--- src/main/java/com/dramancompany/taxiServiceBe/common/ResponseExceptionHandler.java
+++ src/main/java/com/dramancompany/taxiServiceBe/common/ResponseExceptionHandler.java
@@ -3,6 +3,7 @@ package com.dramancompany.taxiServiceBe.common;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -39,6 +40,15 @@ public class ResponseExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST.value())
                 .message("필드 정보가 잘못되었습니다.")
                 .errors(ErrorResponse.FieldError.of(e.getName(), value, e.getErrorCode()))
+                .build();
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(e.getMessage())
                 .build();
         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
